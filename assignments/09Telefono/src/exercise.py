a7885153d04d5e7b210300ecf3a6b978d62de42c
LordEtanol <a01412682@itesm.mx>
2021-08-27 12:51:32 -0500

    Termine el ejercicio 2

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    mensajes = int(input("Dame el número de mensajes: "))
+    megas = float(input("Dame el número de megas: "))
+    minutos = int(input("Dame el número de minutos: "))
 
+    costo = (mensajes * 0.80) + (megas * 0.80) + (minutos * .80)
+
+    print("El costo mensual es:", costo)
 
 if __name__ == '__main__':
     main()
